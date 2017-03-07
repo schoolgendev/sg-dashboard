5cc38fdf1e0cace445a097a80e08932c90f8306e
Schoolgen <schoolgennz@gmail.com>
2017-03-08 09:33:22 +1300

    avocado.jpg to Avocado.jpg

--- js/pageController.js
+++ js/pageController.js
@@ -714,7 +714,7 @@ $(document).ready(function () {
                 {
                     obj: "wg-avocado",
                     val: 0.215,
-                    bg: "url('img/avocado.jpg')",
+                    bg: "url('img/Avocado.jpg')",
                     color: "#4F4F4F",
                     lText: {
                         margin: {
